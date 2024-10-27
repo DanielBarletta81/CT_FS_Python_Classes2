9ae53a93755fc770ec92f9b956d5db682300c172
DanielBarletta81 <dbarcelona1481@gmail.com>
2024-10-26 20:51:28 -0400

    add file for encapsulation exercise

---
+++ encapsulation.py
@@ -0,0 +1,10 @@
+##
+
+#: You are required to build a Personal Budget Management application. 
+# The application will manage budget categories like food, entertainment, utilities, etc.,
+#  ensuring that budget details remain private and are accessed or modified through public methods.
+
+#Task 1: Define Budget Category Class Create a class `BudgetCategory` 
+# with private attributes for category name and allocated budget. 
+# - Initialize these attributes in the constructor.
+
